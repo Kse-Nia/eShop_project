53dd206f71dc45d70773b9e712ea168cb0534dcf
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-02-02 01:12:36 +0100

    creation components

---
+++ frontend/src/Components/formLogin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const formLogin = () => {
+  return <div></div>;
+};
+
+export default formLogin;
--- frontend/src/Components/itemCard.jsx
+++ frontend/src/Components/itemCard.jsx
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 
 const itemCard = (props) => {
+    const data 
   return (
     <Card maxW="sm">
       <CardBody>
