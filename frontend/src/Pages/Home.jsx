a88dd3e93269f4dd01a6b62d7271977340532596
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-02-02 01:10:45 +0100

    creation Pages

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import data from "../data.json";
+
+const Home = () => {
+  console.log(data);
+
+  return (
+    <div>
+      <p>Hello home</p>
+    </div>
+  );
+};
+
+export default Home;
